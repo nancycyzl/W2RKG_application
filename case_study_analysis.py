ce6e391a3cfa9d5bc5bf8718db93afdead4f8017
NancycyZL <lanzhao944@gmail.com>
2025-07-09 20:13:40 +0800

    add case study evaluation results

--- case_study_analysis.py
+++ case_study_analysis.py
@@ -1,6 +1,20 @@
+'''
+1. Obtain the exchanges only in Maestri, only in our approach, and in both
+2. For those only in our approach, check whether they appear in other Maestri cases + ISDATA (use script to faciliate checking)
+   Saved results in exchanges_only_in_our_analysis.xlsx
+3. Calculate statistics: how many exist in other Maestri cases + ISDATA (unique waste-resource pairs)
+4. for those only in our approach and do not exist in other cases, check how many are correct
+   4.1 Manaully check and save results in exchanges_only_in_our_analysis_label_0_analysis.xlsx
+   4.2 Use script to check the results
+'''
+
 import pandas as pd
 import argparse
 import os
+
+from data_utils.create_maestri_csv import read_maestri_data
+from rapidfuzz import fuzz
+import re
 
 def parse_exchanges(comparison_df):
     '''
@@ -70,19 +84,144 @@ def save_exchanges_three_types(maestri_exchange_list, our_exchange_list, save_fo
     print(f"Number of collaborations in both: {num_collaboration_both}")
 
 
+def obtain_exchanges_in_Maestri(maestri_filepath, exclude_case=None):
+    maestri_df = read_maestri_data(maestri_filepath)
+    if exclude_case is not None:
+        maestri_df = maestri_df[maestri_df["case_id"] != exclude_case]
+
+    exchanges_in_Maestri = []
+    for _, row in maestri_df.iterrows():
+        exchange_id = row["exchange_id"]
+        waste = row["waste"]
+        resource = row["resource"]
+        exchanges_in_Maestri.append([exchange_id, waste, resource])
+    
+    return exchanges_in_Maestri
+
+def obtain_exchanges_in_ISDATA(isdata_filepath):
+    isdata_df = pd.read_excel(isdata_filepath, header=[0,1])
+    isdata_df.columns = ['_'.join(col).strip() for col in isdata_df.columns.values]
+
+    isdata_w2r_df = isdata_df[["Exchange Identifier_(xxxx.x)", "By-product/Waste (CN)_Common name(s)", "Secondary Materials (CN)_Common Name 1, Common name 2"]]
+    isdata_w2r_df.columns = ["exchange_id", "waste", "resource"]
+
+    exchanges_in_ISDATA = []
+    for _, row in isdata_w2r_df.iterrows():
+        if not pd.isna(row["waste"]) and not pd.isna(row["resource"]):
+            exchange_id = row["exchange_id"]
+            waste = row["waste"]
+            resource = row["resource"]
+            exchanges_in_ISDATA.append([exchange_id, waste, resource])
+    
+    return exchanges_in_ISDATA
+
+def preprocess_material_name(s):
+    # Lowercase, remove punctuation, and normalize whitespace
+    s = s.lower()
+    s = re.sub(r'[^a-z0-9\s]', ' ', s)
+    s = re.sub(r'\s+', ' ', s).strip()
+    return s
+
+def compute_material_similarity(str1, str2):
+    # If str1 is empty, return 1
+    if not str1:
+        return 1.0
+    # If str1 is in str2 or str2 is in str1, return 1
+    if str1 in str2 or str2 in str1:
+        return 1.0
+    s1 = preprocess_material_name(str1)
+    s2 = preprocess_material_name(str2)
+    return fuzz.token_set_ratio(s1, s2) / 100.0
+
 def main(args):
-    comparison_df = pd.read_csv(os.path.join(args.base_folder, "comparison_with_maestri.csv"))
-    maestri_exchange_list, our_exchange_list = parse_exchanges(comparison_df)
+    if args.stage == "1":
+        ##### 1. Obtain the exchanges only in Maestri, only in our approach, and in both
+        comparison_df = pd.read_csv(os.path.join(args.base_folder, "comparison_with_maestri.csv"))
+        maestri_exchange_list, our_exchange_list = parse_exchanges(comparison_df)
 
-    print("Number of exchanges in Maestri: ", comparison_df["maestri_num_exchanges"].sum())
-    print("Number of exchanges in our approach: ", comparison_df["final_num_exchanges"].sum())
+        print("Number of exchanges in Maestri: ", comparison_df["maestri_num_exchanges"].sum())
+        print("Number of exchanges in our approach: ", comparison_df["final_num_exchanges"].sum())
 
-    save_exchanges_three_types(maestri_exchange_list, our_exchange_list, save_folder=args.base_folder)
+        save_exchanges_three_types(maestri_exchange_list, our_exchange_list, save_folder=args.base_folder)
 
+    elif args.stage == "2":
+        ##### 2. For those only in our approach, check whether they appear in other Maestri cases + ISDATA
+        maestri_other_exchanges = obtain_exchanges_in_Maestri("data_utils/Maestri.xlsx", exclude_case=1)
+        isdata_exchanges = obtain_exchanges_in_ISDATA("data_utils/ISDATA.xlsx")
+        total_ref_exchanges = maestri_other_exchanges + isdata_exchanges
+        print("Number of exchanges in Maestri: ", len(maestri_other_exchanges))
+        print("Number of exchanges in ISDATA: ", len(isdata_exchanges))
+        print("Number of total reference exchanges: ", len(total_ref_exchanges))
+
+        contune_query = True
+        while contune_query:
+            waste = input("Enter the waste: ")
+            resource = input("Enter the resource: ")
+            if waste == "q" or resource == "q":
+                break
+            
+            # filter references
+            for exchange_id, ref_waste, ref_resource in total_ref_exchanges:
+                if (compute_material_similarity(waste, ref_waste) > args.material_similarity_threshold and
+                    compute_material_similarity(resource, ref_resource) > args.material_similarity_threshold):
+                    print(f"Exchange ID {exchange_id}: {ref_waste} -> {ref_resource}")
+
+    elif args.stage == "3":
+        ##### 3. Calculate statistics: how many exist in other Maestri cases + ISDATA (unique waste-resource pairs)
+        exchanges_only_in_our_df = pd.read_excel(os.path.join(args.base_folder, "exchanges_only_in_our_analysis.xlsx"))
+
+        # Find rows with same waste-resource combination but different labels
+        duplicates = exchanges_only_in_our_df.groupby(['waste', 'resource'])['exist in other cases'].nunique()
+        inconsistent_pairs = duplicates[duplicates > 1].index
+        
+        if len(inconsistent_pairs) > 0:
+            print("\nFound inconsistent labels for these waste-resource pairs:")
+            for waste, resource in inconsistent_pairs:
+                rows = exchanges_only_in_our_df[
+                    (exchanges_only_in_our_df['waste'] == waste) & 
+                    (exchanges_only_in_our_df['resource'] == resource)
+                ]
+                print(f"\nWaste: {waste}")
+                print(f"Resource: {resource}") 
+                print("Labels:", rows['exist in other cases'].tolist())
+        else:
+            print("\nAll labels are consistent for all waste-resource pairs")
+
+        # Count unique waste-resource combinations
+        unique_combinations = exchanges_only_in_our_df[['waste', 'resource']].drop_duplicates()
+        print(f"Number of unique waste-resource combinations: {len(unique_combinations)}")
+        print(f"Number of total waste-resource pairs: {len(exchanges_only_in_our_df)}")
+
+        # Count unique waste-resource combinations with label = 1
+        unique_combinations_with_label_1 = exchanges_only_in_our_df[exchanges_only_in_our_df['exist in other cases'] == 1][['waste', 'resource']].drop_duplicates()
+        print(f"Number of unique waste-resource combinations with label = 1: {len(unique_combinations_with_label_1)}")
+        print(f"Number of waste-resource pairs with label = 1: {len(exchanges_only_in_our_df[exchanges_only_in_our_df['exist in other cases'] == 1])}")
+
+        # Count unique waste-resource combinations with label = 0
+        unique_combinations_with_label_0 = exchanges_only_in_our_df[exchanges_only_in_our_df['exist in other cases'] == 0][['waste', 'resource']].drop_duplicates()
+        print(f"Number of unique waste-resource combinations with label = 0: {len(unique_combinations_with_label_0)}")
+        print(f"Number of waste-resource pairs with label = 0: {len(exchanges_only_in_our_df[exchanges_only_in_our_df['exist in other cases'] == 0])}")
+
+        # Count unique waste-resource combinations with label = /
+        unique_combinations_with_label_unknown = exchanges_only_in_our_df[exchanges_only_in_our_df['exist in other cases'] == "/"][['waste', 'resource']].drop_duplicates()
+        print(f"Number of unique waste-resource combinations with label = /: {len(unique_combinations_with_label_unknown)}")
+        label_unknown = exchanges_only_in_our_df[exchanges_only_in_our_df['exist in other cases'] == '/']
+        print(f"Number of waste-resource pairs with label = /: {len(label_unknown)}")
+
+        # save the unique waste-resource pairs with label = 0
+        unique_combinations_with_label_0.to_csv(os.path.join(args.base_folder, "exchanges_only_in_our_analysis_label_0.csv"), index=False)
+
+    elif args.stage == "4":
+        ##### 4. for those only in our approach and do not exist in other cases, check how many are correct
+        df = pd.read_excel(os.path.join(args.base_folder, "exchanges_only_in_our_analysis_label_0_analysis.xlsx"))
+        print("Number of correct waste-to-resource pairs: ", df[df["correct"] == 1].shape[0])
+        print("Number of incorrect waste-to-resource pairs: ", df[df["correct"] == 0].shape[0])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--base_folder", type=str, default="case_study1/threshold_0.75")
+    parser.add_argument("--stage", type=str, default="4", choices=["1", "2", "3", "4"])
+    parser.add_argument("--material_similarity_threshold", type=float, default=0.7)
     args = parser.parse_args()
     main(args)
 
